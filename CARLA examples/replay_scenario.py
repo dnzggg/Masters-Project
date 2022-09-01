164705cf94ad594a7b1b54475e8fcf41b5b0b000
deniz <zfac242@live.rhul.ac.uk>
2022-09-01 13:48:55 +0100

    Robustness signals of all scenarios collected and recorded into json files. Changed the VerificationOfExperiments.py to calculate safety properties of every rule mentioned in the report.

--- CARLA examples/replay_scenario.py
+++ CARLA examples/replay_scenario.py
@@ -26,39 +26,39 @@ def main():
     settings.fixed_delta_seconds = 0.05
     world.apply_settings(settings)
 
-    file = "C:/Users/Deniz Gorur/PycharmProjects/Masters Project/Scenic examples/records/scenario1.log"
-    info = client.show_recorder_file_info(file, True)
-    frames = int(re.search(r"Frames: (\d+)", info).group(1))
-    print(client.show_recorder_actors_blocked(file, 0, 10))
+    file = "C:/Users/Deniz Gorur/PycharmProjects/Masters Project/records/scenario1.log"
+    # info = client.show_recorder_file_info(file, True)
+    # frames = int(re.search(r"Frames: (\d+)", info).group(1))
+    # print(client.show_recorder_actors_blocked(file, 0, 10))
     client.replay_file(file, 0, 0, 442)
-
+    #
     world.tick()
-
-    blueprint_library = world.get_blueprint_library()
-    vehicle = list(world.get_actors().filter('vehicle.tesla.model3'))[0]
-    print(vehicle)
-
-    rgb_camera_sensor = blueprint_library.find('sensor.camera.rgb')
-    rgb_camera_sensor.set_attribute('image_size_x', '1920')
-    rgb_camera_sensor.set_attribute('image_size_y', '1080')
-    rgb_camera_sensor.set_attribute('fov', '110')
-    cam_location = carla.Location(0.8, 0, 1.7)
-    cam_rotation = carla.Rotation(0, 0, 0)
-    cam_transform = carla.Transform(cam_location, cam_rotation)
-    camera = world.spawn_actor(rgb_camera_sensor, cam_transform, attach_to=vehicle,
-                               attachment_type=carla.AttachmentType.Rigid)
-
-    image_queue = queue.Queue()
-    camera.listen(image_queue.put)
-    for i in range(frames * 2):
-        world.tick()
-        image = image_queue.get()
-        print(vehicle.get_location())
-        print(vehicle.get_acceleration())
-        w = world.get_snapshot()
-        print(w.timestamp)
-        if i % 20 == 0:
-            image.save_to_disk('%s/%s.png' % (camera.type_id, image.frame))
+    #
+    # blueprint_library = world.get_blueprint_library()
+    # vehicle = list(world.get_actors().filter('vehicle.tesla.model3'))[0]
+    # print(vehicle)
+    #
+    # rgb_camera_sensor = blueprint_library.find('sensor.camera.rgb')
+    # rgb_camera_sensor.set_attribute('image_size_x', '1920')
+    # rgb_camera_sensor.set_attribute('image_size_y', '1080')
+    # rgb_camera_sensor.set_attribute('fov', '110')
+    # cam_location = carla.Location(0.8, 0, 1.7)
+    # cam_rotation = carla.Rotation(0, 0, 0)
+    # cam_transform = carla.Transform(cam_location, cam_rotation)
+    # camera = world.spawn_actor(rgb_camera_sensor, cam_transform, attach_to=vehicle,
+    #                            attachment_type=carla.AttachmentType.Rigid)
+    #
+    # image_queue = queue.Queue()
+    # camera.listen(image_queue.put)
+    # for i in range(frames * 2):
+    #     world.tick()
+    #     image = image_queue.get()
+    #     print(vehicle.get_location())
+    #     print(vehicle.get_acceleration())
+    #     w = world.get_snapshot()
+    #     print(w.timestamp)
+    #     if i % 20 == 0:
+    #         image.save_to_disk('%s/%s.png' % (camera.type_id, image.frame))
 
     vehicles = world.get_actors().filter('vehicle.*')
     sensors = world.get_actors().filter('sensors.*')
@@ -66,6 +66,7 @@ def main():
         a.destroy()
     for a in sensors:
         a.destroy()
+    world.tick()
 
 
 if __name__ == '__main__':
--- VerificationOfExperiments.py
+++ VerificationOfExperiments.py
@@ -4,9 +4,9 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
+from srunner.metrics.tools.metrics_log import MetricsLog
 
 import STL
-from srunner.metrics.tools.metrics_log import MetricsLog
 
 try:
     sys.path.append(glob.glob('carla-*%d.%d-%s.egg' % (
@@ -17,6 +17,7 @@ except IndexError:
     pass
 
 import carla
+import json
 
 max_longitudinal_acceleration = 3.5
 min_longitudinal_brake = 4.0
@@ -43,7 +44,7 @@ def calculate_minimal_safe_lateral_distance(log, ego, adv, j):
     velocity = np.array([vel.x, vel.y, vel.z])
     v_1 = (ego_rot @ velocity)[1]
 
-    vel = adv.get_velocity()
+    vel = log.get_actor_velocity(adv.id, j)
     velocity = np.array([vel.x, vel.y, vel.z])
     v_2 = (adv_rot @ velocity)[1]
 
@@ -61,20 +62,28 @@ def calculate_minimal_safe_longitudinal_distance(log, ego, adv, j):
     ego_id = ego.id
     ego_rot, adv_rot = get_rotation_matrix(log, ego_id, adv, j)
 
-    # try get_up_vector for longitudinal velocity
     vel = log.get_actor_velocity(ego_id, j)
     velocity = np.array([vel.x, vel.y, vel.z])
     v_h = (ego_rot @ velocity)[0]
 
-    vel = adv.get_velocity()
+    accel = log.get_actor_acceleration_variation(ego_id, j)
+    acceleration = np.array([accel.x, accel.y, accel.z])
+    a_h = (ego_rot @ acceleration)[0]
+
+    vel = log.get_actor_velocity(adv.id, j)
     velocity = np.array([vel.x, vel.y, vel.z])
     v_p = (adv_rot @ velocity)[0]
+
+    accel = log.get_actor_acceleration_variation(adv.id, j)
+    acceleration = np.array([accel.x, accel.y, accel.z])
+    a_p = (adv_rot @ acceleration)[0]
 
     delta_time = log.get_delta_time(j)
     safe_distance = max(0, v_h * delta_time + 0.5 * max_longitudinal_acceleration * (delta_time ** 2)
                         + ((v_h + delta_time * max_longitudinal_acceleration) ** 2) / (2 * min_longitudinal_brake)
                         - (v_p ** 2) / (2 * max_longitudinal_brake))
-    return safe_distance
+
+    return safe_distance, v_p, v_h, a_p, a_h
 
 
 # Works with only two vehicles
@@ -89,118 +98,200 @@ def main():
     settings.fixed_delta_seconds = 0.05
     world.apply_settings(settings)
 
-    file = os.path.dirname(__file__) + "/records/transfuserRouteScenario_3_rep2.log"
-    info = client.show_recorder_file_info(file, True)
+    os.chdir('records')
+    for file in sorted(glob.glob("*.log")):
+        print(file.strip('.log'))
+        if len(glob.glob(file.strip('.log') + '.json')) != 0:
+            continue
+        file = os.path.dirname(__file__) + "/records/" + file
+        info = client.show_recorder_file_info(file, True)
 
-    log = MetricsLog(info)
+        log = MetricsLog(info)
 
-    ego_id = log.get_ego_vehicle_id()
+        ego_id = log.get_ego_vehicle_id()
 
-    robustness_lon = []
-    robustness_lat = []
-    robustness_1 = []
-    robustness_2 = []
-    sig = {"x": [], "y": [], "z": [], "t": [], "a": [], "p": []}
-    time = []
+        x = STL.parse('(x<0)')  # x is the longitudinal distance - safe longitudinal distance
+        u = STL.parse('(u<0.9)')  # u is the lateral distance - safe lateral distance
+        y = STL.parse('(y<1.5)')  # y is the relative difference between velocities
+        z = STL.parse('(z<25)')  # z is the distance
+        t = STL.parse('t')  # t is if the ego vehicle is decelerating due to traffic light
 
-    start_ego, end_ego = log.get_actor_alive_frames(ego_id)
-    start = start_ego
-    end = end_ego - 1
-    duration = log.get_platform_time(end) - log.get_platform_time(start)
-    points = end - start
-    time_period = duration
+        time_headway = 1.5
+        vehicle_mass = 1200
+        g = -0.25 * (time_headway + 1) / vehicle_mass
 
-    vehicles = world.get_actors().filter('vehicle.*')
-    sensors = world.get_actors().filter('sensors.*')
-    for a in vehicles:
-        a.destroy()
-    for a in sensors:
-        a.destroy()
-    world.tick()
+        a = STL.parse(f'(a<{g})')  # a is the acceleration of the ego vehicle
+        p = STL.parse(f'(p<{g})')  # p is the acceleration of the adversary vehicle
+        phi_lon = x
+        phi_lat = u
+        phi2 = ~y | z | t
+        phi3 = ~a | p.historically(lo=0, hi=3)
 
-    client.replay_file(file, 0, 0, ego_id, True)
+        robustness_lon = []
+        robustness_lat = []
+        robustness_1 = []
+        robustness_2 = []
+        sig = {"x": [], "u": [], "y": [], "z": [], "t": [], "a": [], "p": []}
+        time = []
 
-    for i in range(start):
+        start_ego, end_ego = log.get_actor_alive_frames(ego_id)
+        start = start_ego
+        end = end_ego - 1
+        duration = log.get_platform_time(end) - log.get_platform_time(start)
+        points = end - start
+        time_period = duration
+
+        client.replay_file(file, 0, 0, ego_id, False)
+
+        for i in range(start):
+            world.tick()
+
+        actor_list = world.get_actors()
+        ego_vehicle = actor_list.find(ego_id)
+
+        scenario_vehicle_ids = log.get_actor_ids_with_role_name("scenario")
+
+        for i in range(points):
+            j = i + start
+            actor_list = world.get_actors().filter("*vehicle*")
+
+            for vehicle_id in scenario_vehicle_ids:
+                if transform := log.get_actor_transform(vehicle_id, j):
+                    if transform.location.distance(ego_vehicle.get_location()) < 15:
+                        print("Scenario", vehicle_id)
+
+            time.append(log.get_platform_time(j))
+
+            closest_vehicle_in_front = (None, float('inf'))
+            closest_vehicle_side = (None, float('inf'))
+            waypoint = world.get_map().get_waypoint(ego_vehicle.get_location(), project_to_road=False,
+                                                    lane_type=carla.LaneType.Driving | carla.LaneType.Shoulder)
+            transform_ego = ego_vehicle.get_transform()
+
+            for a in actor_list:
+                if a.id != ego_id:
+                    waypoint_adv = world.get_map().get_waypoint(a.get_location(), project_to_road=False,
+                                                                lane_type=carla.LaneType.Driving)
+                    transform_adv = a.get_transform()
+                    distance = transform_ego.location.distance(transform_adv.location)
+                    if (rot := transform_ego.rotation.yaw) < 0:
+                        rot = 360 + transform_ego.rotation.yaw
+
+                    try:
+                        if waypoint.road_id == waypoint_adv.road_id and waypoint.lane_id == waypoint_adv.lane_id:
+                            if rot < 70 or rot >= 340:
+                                if transform_ego.location.x < transform_adv.location.x:
+                                    if closest_vehicle_in_front[1] > distance:
+                                        closest_vehicle_in_front = a, distance
+                            elif 70 <= rot < 160:
+                                if transform_ego.location.y < transform_adv.location.y:
+                                    if closest_vehicle_in_front[1] > distance:
+                                        closest_vehicle_in_front = a, distance
+                            elif 160 <= rot < 250:
+                                if transform_ego.location.x > transform_adv.location.x:
+                                    if closest_vehicle_in_front[1] > distance:
+                                        closest_vehicle_in_front = a, distance
+                            elif 250 <= rot < 340:
+                                if transform_ego.location.y > transform_adv.location.y:
+                                    if closest_vehicle_in_front[1] > distance:
+                                        closest_vehicle_in_front = a, distance
+                    except AttributeError:
+                        pass
+
+                    distance_y = abs(transform_adv.location.y - transform_ego.location.y)
+                    distance_x = abs(transform_adv.location.x - transform_ego.location.x)
+                    try:
+                        if abs(waypoint.lane_id) == 1:
+                            move = int(waypoint.lane_id / abs(waypoint.lane_id))
+                        elif abs(waypoint.lane_id) == 2:
+                            move = int(-waypoint.lane_id / abs(waypoint.lane_id))
+
+                        if waypoint.road_id == waypoint_adv.road_id and waypoint.lane_id + move == waypoint_adv.lane_id:
+                            if rot < 70 or rot >= 340:
+                                if closest_vehicle_side[1] > distance_y:
+                                    closest_vehicle_side = a, distance_y
+                            elif 70 <= rot < 160:
+                                if closest_vehicle_side[1] > distance_x:
+                                    closest_vehicle_side = a, distance_x
+                            elif 160 <= rot < 250:
+                                if closest_vehicle_side[1] > distance_y:
+                                    closest_vehicle_side = a, distance_y
+                            elif 250 <= rot < 340:
+                                if closest_vehicle_side[1] > distance_x:
+                                    closest_vehicle_side = a, distance_x
+                    except AttributeError:
+                        pass
+            try:
+                lane_width = waypoint.lane_width / 2
+                if closest_vehicle_side[0] is not None:
+                    safe_lateral_distance = calculate_minimal_safe_lateral_distance(log, ego_vehicle,
+                                                                                    closest_vehicle_side[0], j)
+                    sd_lat = safe_lateral_distance + lane_width
+                    sig["u"].append(closest_vehicle_side[1] - sd_lat)
+                else:
+                    sig["u"].append(0.9)
+            except AttributeError:
+                sig["u"].append(0.9)
+            if closest_vehicle_in_front[0] is not None:
+                safe_longitudinal_distance, v_p, v_h, a_p, a_h = calculate_minimal_safe_longitudinal_distance(log,
+                                                                    ego_vehicle, closest_vehicle_in_front[0], j)
+                sig["x"].append(closest_vehicle_in_front[1] - safe_longitudinal_distance)
+
+                sig["y"].append(v_p - v_h)
+                sig["z"].append(ego_vehicle.get_location().distance(closest_vehicle_in_front[0].get_location()))
+                sig["t"].append(ego_vehicle.is_at_traffic_light())
+                sig["a"].append(a_p)
+                sig["p"].append(a_h)
+            else:
+                sig["x"].append(0)
+                sig["y"].append(1.5)
+                sig["z"].append(25)
+                sig["t"].append(0)
+                sig["a"].append(0)
+                sig["p"].append(0)
+
+            val = STL.evaluate(phi_lon, sig, time, t=i, points=points, time_period=time_period)
+            val = 1 if val == float('inf') else val
+            val = 0 if val == float('-inf') else val
+            robustness_lon.append(val)
+            val = STL.evaluate(phi_lat, sig, time, t=i, points=points, time_period=time_period)
+            val = 1 if val == float('inf') else val
+            val = 0 if val == float('-inf') else val
+            robustness_lat.append(val)
+            val = STL.evaluate(phi2, sig, time, t=i, points=points, time_period=time_period)
+            val = 1 if val == float('inf') else val
+            val = 0 if val == float('-inf') else val
+            robustness_1.append(val)
+            val = STL.evaluate(phi3, sig, time, t=i, points=points, time_period=time_period)
+            val = 1 if val == float('inf') else val
+            val = 0 if val == float('-inf') else val
+            robustness_2.append(val)
+            world.tick()
+
+        client.stop_replayer(keep_actors=False)
+        vehicles = world.get_actors().filter('vehicle.*')
+        sensors = world.get_actors().filter('sensors.*')
+        for a in vehicles:
+            a.destroy()
+        for a in sensors:
+            a.destroy()
         world.tick()
 
-    actor_list = world.get_actors()
-    ego_vehicle = actor_list.find(ego_id)
+        with open(file.strip('.log') + ".json", 'w', encoding='utf-8') as f:
+            json.dump({"robustness_lon": {"sig": robustness_lon, "max": max(robustness_lon), "min": min(robustness_lon), "avg": sum(robustness_lon) / len(robustness_lon)},
+                       "robustness_lat": {"sig": robustness_lat, "max": max(robustness_lat), "min": min(robustness_lat), "avg": sum(robustness_lat) / len(robustness_lat)},
+                       "robustness_1": {"sig": robustness_1, "max": max(robustness_1), "min": min(robustness_1), "avg": sum(robustness_1) / len(robustness_1)},
+                       "robustness_2": {"sig": robustness_2, "max": max(robustness_2), "min": min(robustness_2), "avg": sum(robustness_2) / len(robustness_2)}},
+                      f, indent=4)
 
-    scenario_vehicle_ids = log.get_actor_ids_with_role_name("scenario")
-
-    for i in range(points):
-        j = i + start
-        actor_list = world.get_actors().filter("*vehicle*")
-
-        for vehicle_id in scenario_vehicle_ids:
-            if transform := log.get_actor_transform(vehicle_id, j):
-                if transform.location.distance(ego_vehicle.get_location()) < 15:
-                    print("Scenario", vehicle_id)
-
-        closest_vehicle_in_front = (None, float('inf'))
-        closest_vehicle_side = (None, float('inf'))
-        waypoint = world.get_map().get_waypoint(ego_vehicle.get_location(), project_to_road=False,
-                                                lane_type=carla.LaneType.Driving | carla.LaneType.Shoulder)
-        for a in actor_list:
-            if a.id != ego_id:
-                waypoint_adv = world.get_map().get_waypoint(a.get_location(), project_to_road=False,
-                                                            lane_type=carla.LaneType.Driving)
-                try:
-                    if waypoint.road_id == waypoint_adv.road_id and waypoint.lane_id == waypoint_adv.lane_id:
-                        if waypoint.transform.rotation.yaw < 80:
-                            if waypoint.transform.location.x < waypoint_adv.transform.location.x:
-                                distance = waypoint_adv.transform.location.distance(waypoint.transform.location)
-                                if closest_vehicle_in_front[1] > distance:
-                                    closest_vehicle_in_front = a, distance
-                        elif 80 <= waypoint.transform.rotation.yaw < 170:
-                            if waypoint.transform.location.y < waypoint_adv.transform.location.y:
-                                distance = waypoint_adv.transform.location.distance(waypoint.transform.location)
-                                if closest_vehicle_in_front[1] > distance:
-                                    closest_vehicle_in_front = a, distance
-                        elif 170 <= waypoint.transform.rotation.yaw < 260:
-                            if waypoint.transform.location.x > waypoint_adv.transform.location.x:
-                                distance = waypoint_adv.transform.location.distance(waypoint.transform.location)
-                                if closest_vehicle_in_front[1] > distance:
-                                    closest_vehicle_in_front = a, distance
-                        elif waypoint.transform.rotation.yaw >= 260:
-                            if waypoint.transform.location.y > waypoint_adv.transform.location.y:
-                                distance = waypoint_adv.transform.location.distance(waypoint.transform.location)
-                                if closest_vehicle_in_front[1] > distance:
-                                    closest_vehicle_in_front = a, distance
-                except AttributeError:
-                    pass
-                try:
-                    if waypoint.road_id == waypoint_adv.road_id and waypoint.lane_id - 1 == waypoint_adv.lane_id:
-                        distance = waypoint_adv.transform.location.distance(waypoint.transform.location)
-                        if closest_vehicle_side[1] > distance:
-                            closest_vehicle_side = a, distance
-                except AttributeError:
-                    pass
-        try:
-            lane_width = waypoint.lane_width / 2
-            if closest_vehicle_side[0] is not None:
-                safe_lateral_distance = calculate_minimal_safe_lateral_distance(log, ego_vehicle, closest_vehicle_side[0],
-                                                                                j)
-                print(safe_lateral_distance + lane_width)
-                print(closest_vehicle_side[1])
-        except AttributeError:
-            pass
-        if closest_vehicle_in_front[0] is not None:
-            print("In front", closest_vehicle_in_front[0].id)
-            safe_longitudinal_distance = calculate_minimal_safe_longitudinal_distance(log, ego_vehicle,
-                                                                                      closest_vehicle_in_front[0], j)
-            print(safe_longitudinal_distance)
-
-        world.tick()
-
-    client.stop_replayer(keep_actors=False)
-    vehicles = world.get_actors().filter('vehicle.*')
-    sensors = world.get_actors().filter('sensors.*')
-    for a in vehicles:
-        a.destroy()
-    for a in sensors:
-        a.destroy()
-    world.tick()
+        # plt.plot(time, sig["x"], time, robustness_lon, "r--")
+        # plt.show()
+        # plt.plot(time, sig["u"], time, robustness_lat, "r--")
+        # plt.show()
+        # plt.plot(time, sig["y"], time, sig["z"], time, np.array(sig["t"]) * 5, time, robustness_1, "r--")
+        # plt.show()
+        # plt.plot(time, sig["a"], time, sig["p"], time, robustness_2, "r--")
+        # plt.show()
 
 
 if __name__ == '__main__':
